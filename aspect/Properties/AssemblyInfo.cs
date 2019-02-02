deef43885b3e42463eb1315fcdea39e23820504d
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2019-02-02 12:42:16 -0700

    Initial project setup

---
+++ aspect/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+
+[assembly: AssemblyTitle("aspect")]
+[assembly: AssemblyProduct("aspect")]
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
