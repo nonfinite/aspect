4c5f2d7df56081c596954c06d52ff61d69ff4e06
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2019-02-05 20:43:19 -0700

    Updated search to focus textbox when flyout is opened, and to close flyout on esc

--- aspect/UI/MainWindow.xaml.cs
+++ aspect/UI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 using Aspect.Properties;
 
@@ -21,6 +22,14 @@ namespace Aspect.UI
 
         public MainViewModel ViewModel => mViewModel;
 
+        private void _CloseFlyout(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (sender is Flyout flyout)
+            {
+                flyout.IsOpen = false;
+            }
+        }
+
         private void _HandleBrowseBack(object sender, ExecutedRoutedEventArgs e) => ViewModel.NavBack();
 
         private void _HandleBrowseForward(object sender, ExecutedRoutedEventArgs e) => ViewModel.NavForward();
@@ -36,6 +45,28 @@ namespace Aspect.UI
             if (item != null)
             {
                 mImageList.ScrollIntoView(item);
+            }
+        }
+
+        private void _HandleSearch(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (mImageListFlyout.IsOpen)
+            {
+                mSearchTextBox.Focus();
+            }
+            else
+            {
+                mImageListFlyout.IsOpenChanged += _FocusOnOpen;
+                mImageListFlyout.IsOpen = true;
+            }
+
+            void _FocusOnOpen(object sender3, RoutedEventArgs e3)
+            {
+                if (mImageListFlyout.IsOpen)
+                {
+                    mImageListFlyout.IsOpenChanged -= _FocusOnOpen;
+                    Dispatcher.Invoke(mSearchTextBox.Focus, DispatcherPriority.Input);
+                }
             }
         }
     }
