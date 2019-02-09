010a4b239a678104993f2289fd6f9bd5031efe82
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2019-02-08 20:53:37 -0700

    Release files for v0.1.0

--- build-release.ps1
+++ build-release.ps1
@@ -1,1 +1,8 @@
 msbuild /restore /p:Configuration=Release
+
+Push-Location (Join-Path $PSScriptRoot "releases")
+
+Move-Item "Setup.exe" "AspectSetup.exe"
+Move-Item "Setup.msi" "AspectSetup.msi"
+
+Pop-Location
