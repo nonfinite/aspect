c0b9a45f60009e4b666c887717f9e322beda8731
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2019-02-09 10:50:56 -0700

    Updated package release step to not be linked to release builds directly

--- build-release.ps1
+++ build-release.ps1
@@ -1,8 +1,12 @@
-msbuild /restore /p:Configuration=Release
+Push-Location $PSScriptRoot
+
+msbuild /restore /p:Configuration=Release /t:aspect:PackageRelease
 
 Push-Location (Join-Path $PSScriptRoot "releases")
 
 Move-Item -Force "Setup.exe" "AspectSetup.exe"
 Move-Item -Force "Setup.msi" "AspectSetup.msi"
+
+Pop-Location
 
 Pop-Location
