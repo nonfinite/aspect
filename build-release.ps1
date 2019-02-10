6e8946e1b994a763f2d6112431d3be6965fd9b5b
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2019-02-10 10:38:04 -0700

    Added application icon

--- build-release.ps1
+++ build-release.ps1
@@ -5,7 +5,6 @@ msbuild /restore /p:Configuration=Release /t:aspect:PackageRelease
 Push-Location (Join-Path $PSScriptRoot "releases")
 
 Move-Item -Force "Setup.exe" "AspectSetup.exe"
-Move-Item -Force "Setup.msi" "AspectSetup.msi"
 
 Pop-Location
 
