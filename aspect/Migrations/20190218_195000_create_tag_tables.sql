2362f772c69c9dcb46ae6e9d27c12a74424eb9b9
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2019-02-18 20:06:29 -0700

    Added tables to store tag information

---
+++ aspect/Migrations/20190218_195000_create_tag_tables.sql
@@ -0,0 +1,10 @@
+﻿create table Tag
+( id   integer not null constraint PK_Tag primary key autoincrement
+, name text    not null constraint UQ_Tag_name unique
+);
+
+create table FileTag
+( file_id integer not null constraint FK_FileTag_File references File(id) on delete cascade
+, tag_id  integer not null constraint FK_FileTag_Tag  references Tag(id)  on delete cascade
+, constraint PK_FileTag primary key (file_id, tag_id)
+);
