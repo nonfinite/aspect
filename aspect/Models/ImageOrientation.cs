4e6522df463eadf1c169ae20583f73813b54c9ee
Nonfinite <39420878+nonfinite@users.noreply.github.com>
2019-02-26 21:19:47 -0700

    Updated images to display correctly based on exif rotation

--- aspect/Models/ImageOrientation.cs
+++ aspect/Models/ImageOrientation.cs
@@ -1,5 +1,7 @@
 namespace Aspect.Models
 {
+    // https://docs.microsoft.com/en-us/windows/desktop/properties/props-system-photo-orientation
+    // NOTE: in practice Rotate90 and 270 seem to be switched from the official documentation
     public enum ImageOrientation : ushort
     {
         Normal = 1,
@@ -7,8 +9,8 @@ namespace Aspect.Models
         Rotate180 = 3,
         FlipVertical = 4,
         Transpose = 5,
-        Rotate270 = 6,
+        Rotate90 = 6,
         Transverse = 7,
-        Rotate90 = 8
+        Rotate270 = 8
     }
 }
